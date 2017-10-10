ca73b6cfc510b37256365554b8765497f4d7d7bd
Phelan Vo <s24471468@gmail.com>
2017-10-10 14:09:55 -0400

    fix indent in home.client.controller.js to fix build errors

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -2,21 +2,20 @@
 
 angular.module('core').controller('HomeController', ['$scope',
     function ($scope) {
-        $scope.myInterval = 3000;
-        $scope.slides = [
-            {
-                image: 'modules/core/client/img/pictures/1.jpg'
-            },
-            {
-                image: 'modules/core/client/img/pictures/2.jpg'
-            },
-            {
-                image: 'modules/core/client/img/pictures/3.jpg'
-            },
-            {
-                image: 'modules/core/client/img/pictures/4.jpg'
-            }
-        ];
-
+      $scope.myInterval = 3000;
+      $scope.slides = [
+        {
+          image: 'modules/core/client/img/pictures/1.jpg'
+        },
+        {
+          image: 'modules/core/client/img/pictures/2.jpg'
+        },
+        {
+          image: 'modules/core/client/img/pictures/3.jpg'
+        },
+        {
+          image: 'modules/core/client/img/pictures/4.jpg'
+        }
+      ];
     }
 ]);
