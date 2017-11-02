6c5e55aa05566c1148121d1b7c433abfbe690098
Phelan Vo <s24471468@gmail.com>
2017-11-01 21:38:29 -0400

    Edit abouts.client.controller and members.client.config for rebase

--- modules/abouts/client/controllers/abouts.client.controller.js
+++ modules/abouts/client/controllers/abouts.client.controller.js
@@ -6,82 +6,73 @@
     .module('abouts')
     .controller('AboutsController', AboutsController);
 
-  AboutsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'aboutResolve'];
-
-  $(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });
-  });
-
+  AboutsController.$inject = ['$scope', '$state', '$window', 'Authentication', 'AboutsService'];
   /*Menu-toggle*/
-  $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("active");
-  });
+  // $("#menu-toggle").click(function(e) {
+  //   e.preventDefault();
+  //   $("#wrapper").toggleClass("active");
+  // });
 
-  /*Scroll Spy*/
-  $('body').scrollspy({ target: '#spy', offset:80});
+
 
   /*Smooth link animation*/
-  $('a[href*=#]:not([href=#])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+  // $('a[href*=#]:not([href=#])').click(function() {
+  //     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
 
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          if (target.length) {
-              $('html,body').animate({
-                  scrollTop: target.offset().top
-              }, 1000);
-              return false;
-          }
-      }
-  });
+  //         var target = $(this.hash);
+  //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  //         if (target.length) {
+  //             $('html,body').animate({
+  //                 scrollTop: target.offset().top
+  //             }, 1000);
+  //             return false;
+  //         }
+  //     }
+  // });
 
-  function AboutsController ($scope, $state, $window, Authentication, about) {
+
+  function AboutsController ($scope, $state, $window, Authentication, AboutsService) {
     var vm = this;
 
     vm.authentication = Authentication;
-    vm.about = about;
+    vm.abouts = AboutsService.query();
     vm.error = null;
     vm.form = {};
-    vm.remove = remove;
-    vm.save = save;
+    // vm.remove = remove;
+    // vm.save = save;
 
-    
 
-    // Remove existing About
-    function remove() {
-      if ($window.confirm('Are you sure you want to delete?')) {
-        vm.about.$remove($state.go('abouts.list'));
-      }
-    }
+    // // Remove existing About
+    // function remove() {
+    //   if ($window.confirm('Are you sure you want to delete?')) {
+    //     vm.about.$remove($state.go('abouts.list'));
+    //   }
+    // }
 
-    // Save About
-    function save(isValid) {
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'vm.form.aboutForm');
-        return false;
-      }
+    // // Save About
+    // function save(isValid) {
+    //   if (!isValid) {
+    //     $scope.$broadcast('show-errors-check-validity', 'vm.form.aboutForm');
+    //     return false;
+    //   }
 
-      // TODO: move create/update logic to service
-      if (vm.about._id) {
-        vm.about.$update(successCallback, errorCallback);
-      } else {
-        vm.about.$save(successCallback, errorCallback);
-      }
+    //   // TODO: move create/update logic to service
+    //   if (vm.about._id) {
+    //     vm.about.$update(successCallback, errorCallback);
+    //   } else {
+    //     vm.about.$save(successCallback, errorCallback);
+    //   }
 
-      function successCallback(res) {
-        $state.go('abouts.view', {
-          aboutId: res._id
-        });
-      }
+    //   function successCallback(res) {
+    //     $state.go('abouts.view', {
+    //       aboutId: res._id
+    //     });
+    //   }
 
-      function errorCallback(res) {
-        vm.error = res.data.message;
-      }
-    }
+    //   function errorCallback(res) {
+    //     vm.error = res.data.message;
+    //   }
+    // }
 
   }
 }());
-
