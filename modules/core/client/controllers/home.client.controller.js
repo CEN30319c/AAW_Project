bdd9068f90fa44f2b5ed1befa568a4b9598bf259
Phelan Vo <s24471468@gmail.com>
2017-11-15 17:07:44 -0500

    Changes to modal function in HomeController

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -21,34 +21,41 @@ angular.module('core').controller('HomeController', ['$scope','$modal', '$log', 
 
     $scope.newslist = NewsService.query();
 
-    $scope.edit = function(header) {
+    /*$scope.edit = function(header) {
       console.log(header);
       modalUpdate(0, header);
-    };
+    };*/
 
-    function modalUpdate(size, header) {
+    $scope.modalUpdate = function(size, texttoedit) {
       var url = 'modules/core/client/views/modal-home.client.view.html';
       var modalInstance = $modal.open({
         templateUrl: url,
-        controller: function ($scope, $modalInstance) {
-
+        controller: function ($scope, $modalInstance, text) {
+          $scope.newtext = text;
+          console.log($scope.newtext);
           $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
           };
 
-          $scope.update = function () {
-
+          $scope.update = function (newtext) {
+            text = newtext;
+            $modalInstance.close(text);
           };
 
         },
         size: size,
+        resolve: {
+          text: function() {
+            return texttoedit;
+          }
+        }
       });
 
       modalInstance.result.then(function () {
       }, function () {
         $log.info('Modal dismissed at: ' + new Date());
       });
-    }
+    };
 
   }
 ]);
