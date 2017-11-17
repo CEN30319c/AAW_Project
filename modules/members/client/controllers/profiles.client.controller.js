3bcfe8e7b4e5cb96fcc486fdd3467813a23735d9
Brandi <bvafiadis@ufl.edu>
2017-11-16 23:23:18 -0500

    profile edit function for admins

--- modules/members/client/controllers/profiles.client.controller.js
+++ modules/members/client/controllers/profiles.client.controller.js
@@ -6,17 +6,38 @@
       .module('members')
       .controller('ProfilesController', ProfilesController);
   
-    ProfilesController.$inject = ['$scope', '$state', '$window', '$modal', '$log', 'MembersService'];
+    ProfilesController.$inject = ['$scope', '$state', '$window', '$modal', '$log', 'MembersService', 'Authentication'];
   
-    function ProfilesController ($scope, $state, $window, $modal, $log, MembersService) {
+    function ProfilesController ($scope, $state, $window, $modal, $log, MembersService, Authentication, member) {
       var vm = this;
+      vm.member = member;
+      vm.authentication = Authentication;
+      $scope.user = Authentication.user;
       $scope.profiles = MembersService.query();
+
+      $scope.ProfileUpdate = function(updatedProfile) {
+        $log.info('help');
+        var profile = updatedProfile;
+
+          profile.$update(function() {
+
+          }, function(errorResponse) {
+              $scope.error = errorResponse.data.message;
+          })
+      }
 
       vm.modalUpdate = function (selectedProfile, size) {
           var modalInstance = $modal.open({
               templateUrl: "modules/members/client/views/profiles-modal.client.view.html",
               controller: function ($scope, $modalInstance, profile) {
                   $scope.profile = profile;
+
+                  $scope.ok = function() {
+                      $modalInstance.close($scope.profile);
+                  };
+                  $scope.cancel = function() {
+                      $modalInstance.dismiss('cancel');
+                  }
               },
               size: size,
                resolve: {
@@ -32,5 +53,33 @@
               $log.info("Modal dismissed at: " + new Date());
           });
       };
+
+      vm.modalEdit = function (selectedProfile, size) {
+        var modalInstance = $modal.open({
+            templateUrl: "modules/members/client/views/profiles-edit-modal.client.view.html",
+            controller: function ($scope, $modalInstance, profile) {
+                $scope.profile = profile;
+
+                $scope.ok = function() {
+                    $modalInstance.close($scope.profile);
+                };
+                $scope.cancel = function() {
+                    $modalInstance.dismiss('cancel');
+                }
+            },
+            size: size,
+             resolve: {
+                 profile: function() {
+                     return selectedProfile;
+                 }
+             }
+        });
+
+        modalInstance.result.then(function(selectedProfile) {
+          $scope.selected = selectedProfile;
+        }, function () {
+            $log.info("Modal dismissed at: " + new Date());
+        });
+    };
     }
   }());
