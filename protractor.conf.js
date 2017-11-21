3d29e3ee34df8cee748f380c82273364acd679e4
Phelan Vo <s24471468@gmail.com>
2017-11-21 16:04:26 -0500

    Fixed grunt issue due to newabouts controller, testing now works with grunt test:e2e

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,8 +2,51 @@
 
 // Protractor configuration
 var config = {
-  specs: ['modules/*/tests/e2e/*.js']
+  specs: ['modules/*/tests/e2e/*.js'],
+
+  onPrepare: function () {
+    var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+    jasmine.getEnv().addReporter(new SpecReporter());
+  }
+
+  /*jasmineNodeOpts: {
+    defaultTimeoutInterval: 20000
+  }*/
 };
+
+/*
+* directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+   // 'browserName': 'firefox'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    isVerbose: true,
+    print: function () {} // remove dots for each test
+  },
+
+  onPrepare: function () {
+    const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+    jasmine.getEnv().addReporter(new SpecReporter());
+},
+
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['signup_test.js','login_test.js'],
+ // specs: ['facultyportal_test.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 20000
+  }
+* */
 
 if (process.env.TRAVIS) {
   config.capabilities = {
