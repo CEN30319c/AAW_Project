0807e34a1e8c616b3fbb601abcca50652ff9d57e
popeil97 <alexpopeil23@gmail.com>
2017-11-22 12:34:26 -0500

    Added misc module, and updates to home module to allow admin to update home page content

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -7,8 +7,13 @@
 //   }
 // ]);
 
-angular.module('core').controller('HomeController', ['$scope','$modal', '$log', 'Authentication', 'NewsService', 'CalendarsService',
-  function ($scope, $modal, $log, Authentication, NewsService, CalendarsService) {
+// <<<<<<< Updated upstream
+angular.module('core').controller('HomeController', ['$scope','$modal', '$log', 'Authentication', 'NewsService', 'CalendarsService', 'MiscsService',
+  function ($scope, $modal, $log, Authentication, NewsService, CalendarsService, MiscsService) {
+// =======
+// angular.module('core').controller('HomeController', ['$scope','$modal', '$log', 'Authentication', 'NewsService', 'MiscsService',
+//   function ($scope, $modal, $log, Authentication, NewsService, MiscsService) {
+// >>>>>>> Stashed changes
     $scope.authentication = Authentication;
     $scope.user = Authentication.user;
     $scope.whoweareText = 'AAW strives to empower UF women for the utmost success in each stage of their careers at the university.';
@@ -18,11 +23,28 @@ angular.module('core').controller('HomeController', ['$scope','$modal', '$log', 
         image: 'modules/core/client/img/pictures/slide5.png'
       }
     ];
-
+    $scope.miscData = MiscsService.query();
+    console.log($scope.miscData);
     $scope.newslist = NewsService.query();
     $scope.calendarlist = CalendarsService.query();
     console.log($scope.calendarlist);
 
+    //var list = $scope.miscData;
+    for(var data in $scope.miscData) {
+      console.log(data);
+    }
+    //console.log($scope.miscData['$promise'][0]);
+    // $scope.homePage_id = '';
+    //
+    //
+    // for(var data in $scope.miscData) {
+    //   // if(data['name'] == 'homePage') {
+    //   //   $scope.homePage_id = data['name'];
+    //   // }
+    //   console.log($scope.miscData[data]['$$state']['value']);
+    // }
+    //
+    // console.log($scope.homePage_id);
     /*$scope.edit = function(header) {
       console.log(header);
       modalUpdate(0, header);
