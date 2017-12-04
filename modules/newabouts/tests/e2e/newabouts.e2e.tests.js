53115351cc030af598dcacb13851ec0ca58f09c6
Brandi <bvafiadis@ufl.edu>
2017-12-04 13:08:46 -0500

    changes in testing

--- modules/newabouts/tests/e2e/newabouts.e2e.tests.js
+++ modules/newabouts/tests/e2e/newabouts.e2e.tests.js
@@ -3,18 +3,14 @@
 describe('Newabouts E2E Tests:', function () {
   describe('Test Newabouts page', function () {
     it('Should report missing credentials', function () {
-      browser.get('http://localhost:3001/newabouts');
+      browser.get('http://localhost:3001/about');
       expect(element.all(by.repeater('newabout in newabouts')).count()).toEqual(0);
     });
 
     it('Should test About page links', function () {
-      browser.get('http://localhost:3001/');
-      var profilesLink = element(by.id('profilesLink'));
-      var awardLink1 = element(by.id('awardLink1'));
-      var awardLink2 = element(by.id('awardLink2'));
-      expect(profilesLink.getAttribute('href')).toEqual('http://localhost:3001/members/profiles');
-      expect(awardLink1.getAttribute('href')).toEqual('http://localhost:3001/about/madelynAward');
-      expect(awardLink2.getAttribute('href')).toEqual('http://localhost:3001/about/distinctionAward');
+      browser.get('http://localhost:3001/about');
+      element(by.buttonText("See All Members")).click();
+      expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/members/profiles');
     });
   });
 });
