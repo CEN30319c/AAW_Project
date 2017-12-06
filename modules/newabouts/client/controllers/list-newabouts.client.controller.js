bfcebe71d98b0453e4ca8f1a6e6a64ab99dd78c3
Phelan Vo <s24471468@gmail.com>
2017-12-05 23:19:48 -0500

    Fix grunt error in newabouts, new grunt build, update aws-sdk info for heroku

--- modules/newabouts/client/controllers/list-newabouts.client.controller.js
+++ modules/newabouts/client/controllers/list-newabouts.client.controller.js
@@ -52,13 +52,13 @@
           var about = updatedAbout;
           //about.text = document.getElementById("description").value;
           about.text = [];
-          if(p1 != '') {
+          if(p1 !== '') {
             about.text.push(p1);
           }
-          if(p2 != '') {
+          if(p2 !== '') {
             about.text.push(p2);
           }
-          if(p3 != '') {
+          if(p3 !== '') {
             about.text.push(p3);
           }
           
